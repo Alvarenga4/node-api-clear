60e1e1791ef7b7eee7b2456102fdf4d1f3f07de6
Francisco Alvarenga <franciscocarlos002@outlook.com>
2021-01-12 14:12:02 -0300

    test: ensure SignUpController return 400 if no name us provider

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
